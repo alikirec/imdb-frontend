2b5755e8d6fb8d04327505bd041b99abb93d8281
Ali Kirecligol <alikirecligol@gmail.com>
2020-02-09 20:49:25 +0300

    arrow

--- src/utils/camelToSnake.ts
+++ src/utils/camelToSnake.ts
@@ -1,7 +1,6 @@
-export default function camelToSnake(str: string): string {
-  return str
+export const camelToSnake = (str: string): string =>
+  str
     .replace(/[\w]([A-Z])/g, function(m) {
       return m[0] + '_' + m[1];
     })
     .toLowerCase();
-}
