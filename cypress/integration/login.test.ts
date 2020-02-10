436381c52ae99c46e636884f9425ad003d80595f
Ali Kirecligol <alikirecligol@gmail.com>
2020-02-10 17:28:26 +0300

    fix cypress tests

--- cypress/integration/login.test.ts
+++ cypress/integration/login.test.ts
@@ -1,5 +1,8 @@
 describe('Authentication test', () => {
   it('should redirect to homepage after successful login', () => {
+    cy.server();
+    console.log(`${Cypress.env('BACKEND_URL')}/auth/login`);
+    cy.route('POST', `${Cypress.env('BACKEND_URL')}/auth/login`).as('login');
     // eslint-disable-next-line @typescript-eslint/ban-ts-ignore
     // @ts-ignore
     cy.clearCookies({ domain: null });
@@ -7,7 +10,9 @@ describe('Authentication test', () => {
     cy.get('[data-cy=username]').type(Cypress.env('USERNAME'));
     cy.get('[data-cy=password]').type(Cypress.env('PASSWORD'));
     cy.get('[data-cy=submit]').click();
-    cy.get('[data-cy=movieFilter]').should('be.visible');
-    cy.get('[data-cy=logout]').should('be.visible');
+    cy.wait('@login').then(() => {
+      cy.get('[data-cy=movieFilter]').should('be.visible');
+      cy.get('[data-cy=logout]').should('be.visible');
+    });
   });
 });
